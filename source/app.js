c833c07d8d1363779c82bfcdd699c94f4599ea69
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-13 20:34:03 +0100

    :construction: Work in progress

--- source/app.js
+++ source/app.js
@@ -3,8 +3,15 @@ if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		//register service worker
 		navigator.serviceWorker
-			.register("../service-worker.js")
-			.then((registered) => console.log(registered));
+			.register("/service-worker.js", {
+				scope: "/",
+			})
+			.then((registered) => console.log(registered))
+			.catch((err) => {
+				const error = { message: err.message };
+				console.log(err);
+				throw error;
+			});
 	});
 }
 console.log("Hello");
