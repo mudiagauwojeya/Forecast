b65f94c55f7fba4de2e653fd71b400b50aa61621
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-13 19:46:14 +0100

    :alembic: Testing

--- source/app.js
+++ source/app.js
@@ -3,7 +3,7 @@ if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		//register service worker
 		navigator.serviceWorker
-			.register("/service-worker.js")
+			.register("../service-worker.js")
 			.then((registered) => console.log(registered));
 	});
 }
