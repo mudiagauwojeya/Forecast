ff1b6f36ea624e02cdb70538925edf2a6c4347a1
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-14 20:09:46 +0100

    :wrench: Configure the service worker install event

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,7 +1,20 @@
 //Service Worker
+
+const cacheName = "sw_v1";
+const precacheFiles = ["./index.html", "./assets/css/*", "./assets/scripts/*"];
+
 self.addEventListener("install", (event) => {
 	//install service-worker
 	console.log(event, "Service worker is installed");
+	event.waitUntil(
+		caches
+			.open(cacheName)
+			.then((cache) => cache.addAll(precacheFiles))
+			.catch((err) => {
+				const error = err.message;
+				throw error;
+			})
+	);
 	self.skipWaiting();
 });
 
