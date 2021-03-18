da1d7c37ebd9cf6261cb7c920d62726ca7158371
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-18 12:44:35 +0100

    :wrench: Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
 						loader: "postcss-loader",
 						options: {
 							postcssOptions: {
-								plugins: [["autoprefixer"]],
+								plugins: [["postcss-preset-env"]],
 							},
 						},
 					},
