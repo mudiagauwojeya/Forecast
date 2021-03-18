0ef016654410d777e00f8dcda09d63511bdf76c3
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-18 13:44:55 +0100

    :zap: Register service worker

--- source/app.js
+++ source/app.js
@@ -1,22 +1,22 @@
 import css from "./css/app.scss";
 
 //service worker
-// if ("serviceWorker" in navigator) {
-// 	window.addEventListener("load", () => {
-// 		//register service worker
-// 		navigator.serviceWorker
-// 			.register("/service-worker.js", {
-// 				scope: "/",
-// 			})
-// 			.then(() => {
-// 				return;
-// 			})
-// 			.catch((err) => {
-// 				const error = { message: err.message };
-// 				console.log(err);
-// 			});
-// 	});
-// }
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", () => {
+		//register service worker
+		navigator.serviceWorker
+			.register("/service-worker.js", {
+				scope: "/",
+			})
+			.then(() => {
+				return;
+			})
+			.catch((err) => {
+				const error = { message: err.message };
+				console.log(err);
+			});
+	});
+}
 
 class App {
 	constructor() {
