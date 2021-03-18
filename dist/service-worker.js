5d5de6e0f948a0e5b234e23beb6af89dc8afa304
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-18 17:51:10 +0100

    :wrench: Disable the service worker config

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -10,36 +10,36 @@ const precacheFiles = [
 	"./assets/scripts/weather.js",
 ];
 
-self.addEventListener("install", (event) => {
-	//install service-worker
-	event.waitUntil(
-		caches
-			.open(cacheName)
-			.then((cache) => cache.addAll(precacheFiles))
-			.catch((err) => {
-				const error = err.message;
-				throw error;
-			})
-	);
-	self.skipWaiting();
-});
+// self.addEventListener("install", (event) => {
+// 	//install service-worker
+// 	event.waitUntil(
+// 		caches
+// 			.open(cacheName)
+// 			.then((cache) => cache.addAll(precacheFiles))
+// 			.catch((err) => {
+// 				const error = err.message;
+// 				throw error;
+// 			})
+// 	);
+// 	self.skipWaiting();
+// });
 
-self.addEventListener("activate", (event) => {
-	//service worker is activated
-	event.waitUntil(
-		caches.keys().then((keys) => {
-			keys.forEach((key) => {
-				if (key !== cacheName) {
-					caches.delete(key);
-				}
-			});
-		})
-	);
-	clients.claim();
-});
+// self.addEventListener("activate", (event) => {
+// 	//service worker is activated
+// 	event.waitUntil(
+// 		caches.keys().then((keys) => {
+// 			keys.forEach((key) => {
+// 				if (key !== cacheName) {
+// 					caches.delete(key);
+// 				}
+// 			});
+// 		})
+// 	);
+// 	clients.claim();
+// });
 
-self.addEventListener("fetch", (event) => {
-	//service worker intercepting fetch request
-	const request = event.request;
-	event.respondWith(caches.match(request).then((response) => response));
-});
+// self.addEventListener("fetch", (event) => {
+// 	//service worker intercepting fetch request
+// 	const request = event.request;
+// 	event.respondWith(caches.match(request).then((response) => response));
+// });
