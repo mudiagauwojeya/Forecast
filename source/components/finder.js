c5bc1016091f76cf238f3a98917e692372756f2b
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-18 14:19:58 +0100

    :bento: Add new module

---
+++ source/components/finder.js
@@ -0,0 +1,18 @@
+class Finder {
+	constructor() {
+		this.lng = coordinates.lng;
+		this.lat = coordinates.lat;
+	}
+
+	getLocation() {
+		if (navigator.geolocation.getCurrentPosition) {
+			navigator.geolocation.getCurrentPosition((coordinates) => {
+				const coords = {
+					lng: coordinates.longitude,
+					lat: coordinates.latitude,
+				};
+				console.log(coords);
+			});
+		}
+	}
+}
