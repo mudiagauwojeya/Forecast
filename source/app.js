d355741652476e14e14d74f260eb88c3628c5e00
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-20 20:13:38 +0100

    :recycle: Refactor the code

--- source/app.js
+++ source/app.js
@@ -1,51 +1,52 @@
 import css from "./css/app.scss";
 
-window.addEventListener("load", () => {
-	class App {
-		constructor() {
-			const getStartedBtn = document.getElementById("getStartedBtn");
-		}
+// service worker
+// if ("serviceWorker" in navigator) {
+// 	window.addEventListener("load", () => {
+// 		//register service worker
+// 		navigator.serviceWorker
+// 			.register("/service-worker.js")
+// 			.then(() => {
+// 				return;
+// 			})
+// 			.catch((err) => {
+// 				const error = { message: err.message };
+// 				console.log(error);
+// 			});
+// 	});
+// }
 
-		init() {
-			// service worker
-			if ("serviceWorker" in navigator) {
-				window.addEventListener("load", () => {
-					//register service worker
-					navigator.serviceWorker
-						.register("/service-worker.js")
-						.then(() => {
-							return;
-						})
-						.catch((err) => {
-							const error = { message: err.message };
-							console.log(error);
-						});
-				});
-			}
-
-			const options = {
-				root: null,
-				rootMargin: "0px 0px -200px 0px",
-				threshold: 0.05,
-			};
-
-			let observer = new IntersectionObserver(fadeIn, options);
-			document.querySelectorAll(".lorem").forEach((lorem) => {
-				observer.observe(lorem);
-			});
-
-			function fadeIn(elements) {
-				elements.forEach((element) => {
-					if (element.isIntersecting) {
-						element.target.classList.add("active");
-					} else {
-						element.target.classList.remove("active");
-					}
-				});
-			}
-		}
+class App {
+	constructor() {
+		this.getStartedBtn = document.getElementById("getStartedBtn");
 	}
 
-	const app = new App();
-	app.init();
-});
+	init() {
+		const options = {
+			root: null,
+			rootMargin: "0px 0px -200px 0px",
+			threshold: 0.05,
+		};
+
+		let observer = new IntersectionObserver(fadeIn, options);
+		document.querySelectorAll(".lorem").forEach((lorem) => {
+			observer.observe(lorem);
+		});
+
+		function fadeIn(elements) {
+			elements.forEach((element) => {
+				if (element.isIntersecting) {
+					element.target.classList.add("active");
+				} else {
+					element.target.classList.remove("active");
+				}
+			});
+		}
+		this.getStartedBtn.addEventListener("click", (e) => {
+			console.log(e);
+		});
+	}
+}
+
+const app = new App();
+app.init();
