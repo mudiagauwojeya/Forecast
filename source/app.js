ce24614ed2a6341948e3682099779a0a4ebf78b3
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-20 19:50:02 +0100

    :recycle: Refactor the code into oop

--- source/app.js
+++ source/app.js
@@ -1,51 +1,51 @@
 import css from "./css/app.scss";
 
-const getStartedBtn = document.getElementById("getStartedBtn");
+window.addEventListener("load", () => {
+	class App {
+		constructor() {
+			const getStartedBtn = document.getElementById("getStartedBtn");
+		}
 
-// service worker
-if ("serviceWorker" in navigator) {
-	window.addEventListener("load", () => {
-		//register service worker
-		navigator.serviceWorker
-			.register("/service-worker.js")
-			.then(() => {
-				return;
-			})
-			.catch((err) => {
-				const error = { message: err.message };
-				console.log(err);
+		init() {
+			// service worker
+			if ("serviceWorker" in navigator) {
+				window.addEventListener("load", () => {
+					//register service worker
+					navigator.serviceWorker
+						.register("/service-worker.js")
+						.then(() => {
+							return;
+						})
+						.catch((err) => {
+							const error = { message: err.message };
+							console.log(error);
+						});
+				});
+			}
+
+			const options = {
+				root: null,
+				rootMargin: "0px 0px -200px 0px",
+				threshold: 0.05,
+			};
+
+			let observer = new IntersectionObserver(fadeIn, options);
+			document.querySelectorAll(".lorem").forEach((lorem) => {
+				observer.observe(lorem);
 			});
-	});
-}
 
-class App {
-	constructor() {
-		this.weather;
+			function fadeIn(elements) {
+				elements.forEach((element) => {
+					if (element.isIntersecting) {
+						element.target.classList.add("active");
+					} else {
+						element.target.classList.remove("active");
+					}
+				});
+			}
+		}
 	}
 
-	init() {}
-}
-
-const app = new App();
-app.init();
-
-const options = {
-	root: null,
-	rootMargin: "0px 0px -200px 0px",
-	threshold: 0.05,
-};
-
-let observer = new IntersectionObserver(fadeIn, options);
-document.querySelectorAll(".lorem").forEach((lorem) => {
-	observer.observe(lorem);
+	const app = new App();
+	app.init();
 });
-
-function fadeIn(elements) {
-	elements.forEach((element) => {
-		if (element.isIntersecting) {
-			element.target.classList.add("active");
-		} else {
-			element.target.classList.remove("active");
-		}
-	});
-}
