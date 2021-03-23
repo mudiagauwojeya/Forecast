4c0969c0ed89433c45734223891544231aeb98b2
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-03-23 19:20:01 +0100

    :construction: Add a method to the weather class

--- source/components/weather.js
+++ source/components/weather.js
@@ -3,4 +3,8 @@ class Weather {
 		this.lng = coordinates.lng;
 		this.lat = coordinates.lat;
 	}
+
+	getWeatherReport() {
+		console.log(`Getting weather data for ${this.lng} and ${this.lat}`);
+	}
 }
